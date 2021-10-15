b2173f0d9b40b89a0eef8e87d17fec75bd767db5
JaewookLee <ghimmk@gmail.com>
2021-10-15 18:10:02 +0900

    Add running time and distance recording feature.

--- shasuri/commit.sh
+++ shasuri/commit.sh
@@ -29,55 +29,63 @@ touch $DATE
 
 # write exercise record in date file
 echo
+
 while : ; do
-    echo "Select Exercise"
-    echo "1. both 2. pull-up 3. plank"
+    echo "Select exercises and choose finish"
+    echo "1. running 2. pull-up 3. plank 4. finish"
     echo -n "> "
     read INPUT
     case $INPUT in
         1)
-            echo -n "pull up count ex) 12 : "
-            read PULL_UP_COUNT
-            echo -n "plank time ex) 1m30s : "
-            read PLANK_TIME
-            echo "pull-up : $PULL_UP_COUNT" > ./$DATE
-            echo "plank : $PLANK_TIME" >> ./$DATE
-            EXERCISE="pull-up, plank"
+            echo -n "running time ex) 15m30s : "
+            read RUNNING_TIME
+            echo -n "running distance ex) 2.3km : "
+            read RUNNING_DIST
+            echo "running : $RUNNING_TIME $RUNNING_DIST" >> ./$DATE
+            EXERCISE=$EXERCISE"running "
             ;;
         2)
             echo -n "pull up count ex) 12 : "
             read PULL_UP_COUNT
-            echo "pull-up : $PULL_UP_COUNT" > ./$DATE
-            EXERCISE="pull-up"
+            echo "pull-up : $PULL_UP_COUNT" >> ./$DATE
+            EXERCISE=$EXERCISE"pull-up "
             ;;
         3)
             echo -n "plank time ex) 1m30s : "
             read PLANK_TIME
-            echo "plank : $PLANK_TIME" > ./$DATE
-            EXERCISE="pull-up"
+            echo "plank : $PLANK_TIME" >> ./$DATE
+            EXERCISE=$EXERCISE"plank "
+            ;;
+        4)
+            echo -n "Fin."
+            break
             ;;
         *)
             echo "Wrong Input\n"
             continue
             ;;
     esac
-    echo
-    echo "----result----"
-    cat ./$DATE
-    echo "--------------"
+done
+
+echo
+echo "----result----"
+cat ./$DATE
+echo "--------------"
+
+while : ; do
     echo "1.confirm 2.cancel"
     echo -n "> "
-	read INPUT
+    read INPUT
     case $INPUT in
-		1)
-			break
-			;;
-		2)
-			;;
-		*)
-			echo "Wrong Input\n"
-			;;
-	esac
+        1)
+            ;;
+        2)
+            exit 1
+            ;;
+        *)
+            echo "Wrong Input\n"
+            ;;
+    esac
 done
 
 # git add & commit
@@ -103,3 +111,5 @@ while : ; do
 			;;
 	esac
 done
+
+exit 0
